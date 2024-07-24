53a033515229eafa55d4160ce2b05a880ca83a2c
Santrupti <behera.santrupti02@gmail.com>
2024-07-24 09:21:27 +0530

    Add files via upload

--- code4.m
+++ code4.m
@@ -1,103 +1,46 @@
-% ID - 0321
-p_trans = [0 0 0 1; 0 1 0 0; 0 0 0 1];
-r = size(p_trans, 1);
-I_1 = eye(r);
-H = [p_trans I_1]; % Parity check matrix
-n = size(H, 2);
+% Simulation parameters
+err = 0.9; % Error probability = 0.(length of my first name = Santrupti)
+range = 100:100:20000; % Range of total bits transmitted from 100-20000 in steps of 100
 
-p = p_trans';
-k = n - r;
-I_2 = eye(k);
-G = [I_2 p];
+% Preallocate arrays for storing results
+num_errors = zeros(length(range), 1);
+error_ratios = zeros(length(range), 1);
 
-
-% Generate(U) a vector of decimal numbers from 0 to 2^k - 1
-dec_numbers = 0:(2^k - 1);
-% Convert each decimal number to a binary string
-bin_strings = dec2bin(dec_numbers);
-% Convert each binary string to a binary vector
-U = bin_strings - '0';
-increment = 1;
-
-
-% Calculate the codeword for each binary vector and Minimum distance
-codewords = mod(U * G, 2);
-min_distance = inf;
-% Calculate the number of codewords
-num_codewords = size(codewords, 1);
-% Compare each pair of codewords
-for i = 1:increment:num_codewords
-    for j = i+1:increment:num_codewords
-        % Calculate the Hamming distance between codewords(i, :) and codewords(j, :)
-        distance = sum(codewords(i, :) ~= codewords(j, :));
-        
-        % If this distance is smaller than min_distance and not zero, update min_distance
-        if distance < min_distance && distance > 0
-            min_distance = distance;
-            codeword_min_d_1 = codewords(i, :);
-            codeword_min_d_2 = codewords(j, :);
-        end
-    end
+% Simulation
+for i = 1:length(range)
+    total_bits = range(i);
+    
+    % Generate a sequence of binary digits (0s and 1s) with equal probability
+    transmitted_sequence = randi([0, 1], total_bits, 1);
+    
+    % Simulate the binary symmetric channel with error probability 0.9
+    received_sequence = transmitted_sequence;
+    flip_probabilities = rand(total_bits, 1);
+    received_sequence(flip_probabilities < err) = ~transmitted_sequence(flip_probabilities < err);
+    
+    % Calculate the number of errors (flipped bits)
+    num_errors(i) = sum(transmitted_sequence ~= received_sequence);
+    
+    % Calculate the error ratio
+    error_ratios(i) = num_errors(i) / total_bits;
 end
 
+% Plotting the results
+figure;
 
-% Syndrome vector
-% Syndrome vector calculation: S = R * H'
-% Where R is received codeword and H' is the transpose of the parity check matrix
-syndrome_d_1 = mod(codeword_min_d_1 * H', 2);
-syndrome_d_2 = mod(codeword_min_d_2 * H', 2);
+% Plot the number of errors vs. the total number of bits transmitted
+subplot(2, 1, 1);
+plot(range, num_errors, 'b-');
+xlabel('Total Number of Bits Transmitted');
+ylabel('Number of Errors');
+title('Number of Errors vs. Total Number of Bits Transmitted');
 
+% Plot the error ratio vs. the total number of bits transmitted
+subplot(2, 1, 2);
+plot(range, error_ratios, 'r-');
+xlabel('Total Number of Bits Transmitted');
+ylabel('Error Ratio');
+title('Error Ratio vs. Total Number of Bits Transmitted');
 
-% Generate transmitted code from last 4 LSB bits from Information bits
-info_bits = [0 0 0 1];
-transmitted_code = mod(info_bits * G, 2);
-
-
-% Syndrome for the transmitted codeword
-syndrome_transmitted_code = mod(transmitted_code * H', 2);
-
-
-% Flip a bit and find syndrome
-received_code = transmitted_code;
-% Flip 3rd bit from LSB or 5th bit from MSB (ID = 321)
-s = size(transmitted_code, 2);
-received_code(s-3+1) = ~transmitted_code(s-3+1);
-syndrome_received_code = mod(received_code * H', 2);
-
-% E is all possible single bit error codewords all powers of 2 till 2^7
-E = zeros(7, 7);
-for i = 1:7
-    E(i, i) = 1;
-end
-
-for i = 1:increment:size(E, 1)
-    error = E(i, :);
-    %syndrome vector calculation
-    syndrome_error = mod(error * H', 2);
-    if syndrome_received_code == syndrome_error
-        error_position = i;
-        error_vector = E(i, :);
-    end
-end
-
-disp('Codeword with minimum distance:');
-disp(codeword_min_d_1);
-disp('Next codeword with minimum distance:');
-disp(codeword_min_d_2);
-disp(['Minimum distance:', num2str(min_distance)]);
-disp('Syndrome for codeword with minimum distance:');
-disp(syndrome_d_1);
-disp('Syndrome for next codeword with minimum distance:');
-disp(syndrome_d_2);
-disp('Transmitted code:');
-disp(transmitted_code);
-disp('Syndrome for transmitted code:');
-disp(syndrome_transmitted_code);
-disp('Flipped transmitted(Received) code:');
-disp(received_code);
-disp('Syndrome for flipped transmitted(Received) code:');
-disp(syndrome_received_code);
-disp('Error position:');
-disp(error_position);
-disp('Error:');
-disp(error_vector);
+% Display the plots
+grid on;
